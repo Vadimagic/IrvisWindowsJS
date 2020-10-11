33fbdb540fe4f8eeb4377bad023a077bc39a7841
Vadim <vadim.prog.2000@gmail.com>
2020-10-11 23:52:01 +0300

    update forms.js and change gulpfile.js

--- src/js/modules/modules.js
+++ src/js/modules/modules.js
@@ -1,12 +1,14 @@
 import modals from "./modals"
 import modalTime from "./modalTime"
 import tabs from "./tabs"
+import forms from "./forms"
 
 const modules = () => {
 	modals();
 	modalTime('.popup_engineer', 60000);
 	tabs(".glazing_slider", ".glazing_block", ".glazing_content", "active");
 	tabs(".decoration_slider", ".no_click", ".decoration_content > div > div", "after_click");
+	forms()
 }
 
 export default modules
