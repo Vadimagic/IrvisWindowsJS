e88575d5fcfb191b3e3d75d0cd221121f97c21b4
Vadim <vadim.prog.2000@gmail.com>
2020-10-16 23:47:01 +0300

    create modul check number in inputs

--- src/js/modules/changeModalState.js
+++ src/js/modules/changeModalState.js
@@ -1,5 +1,29 @@
-const changeModalState = (state) => {
+import checkNumInputs from './checkNumInputs'
 
+const changeModalState = (state) => {
+	const windowForm = document.querySelectorAll('.balcon_icons_img'),
+			windowWidth = document.querySelector('#width'), 
+			windowHeight = document.querySelector('#height'), 
+			windowType = document.querySelector('#view_type'), 
+			windowProdile = document.querySelectorAll('.checkbox');
+			
+	windowForm.forEach((item, i) => {
+		item.addEventListener('click', () => {
+			state.form = i;
+		})
+	})
+
+	windowWidth.addEventListener('input', () => {
+		state.width = windowWidth.value;
+	})
+
+	windowHeight.addEventListener('input', () => {
+		state.height = windowHeight.value;
+	})
+
+	windowType.addEventListener('click', () => {
+		state.type = i;
+	})
 };
 
 export default changeModalState
